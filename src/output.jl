e9e46396f0aeaf10e5132b33a0c767b23974eda0
Felipe <felipe.avba@gmail.com>
2017-11-29 18:28:46 -0500

    no more out-of-place-transforms

--- src/output.jl
+++ src/output.jl
@@ -1,21 +1,21 @@
 function writeoutput(s::AbstractParameters,init::Integer)
-  copy!(rawreal(s.rhs),rawreal(s.u))
-  #dealias!(s.rhs,s)
-  out_transform!(s.aux,s.rhs,s)
+  mycopy!(s.aux,s.u,s)
+  s.p\s.aux
   write("u1.$init",s.aux.rx)
   write("u2.$init",s.aux.ry)
   write("u3.$init",s.aux.rz)
+  dealias!(s.aux,s)
 end
 
 function writeoutput(s::ScalarParameters,init::Integer)
-  copy!(rawreal(s.rhs),rawreal(s.u))
-  #dealias!(s.rhs,s)
-  out_transform!(s.aux,s.rhs,s)
+  mycopy!(s.aux,s.u,s)
+  s.p\s.aux
   write("u1.$init",s.aux.rx)
   write("u2.$init",s.aux.ry)
   write("u3.$init",s.aux.rz)
+  dealias!(s.aux,s)
   copy!(rawreal(s.ρrhs),rawreal(s.ρ))
-  #dealias!(s.ρrhs,s)
   s.ps\s.ρrhs
   write("rho.$init",s.ρrhs)
+  dealias!(s.ρrhs,s)
 end
