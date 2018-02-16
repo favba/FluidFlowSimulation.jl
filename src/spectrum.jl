301fa2601c8aa745ebba6d8e5107306b8ebc5940
Felipe A. V. de Bragança Alves <felipe.avba@gmail.com>
2018-02-16 02:04:48 -0500

    Apply scaling factor

--- src/spectrum.jl
+++ src/spectrum.jl
@@ -4,6 +4,7 @@
   # Initialize the shells to zeros
   fill!(Ef,0)
   dk = kx[2] - kx[1] 
+  scale = 1/(Nxr*Ny*Nz)
   @inbounds for j=1:Ny
     conjFactX=1.0
     for i=1:Nx
@@ -11,7 +12,7 @@
       if n <= Nx
         #magsq = a1.c[i,j,0].r*b1.c[i,j,0].r + a1.c[i,j,0].i*b1.c[i,j,0].i;
         #magsq += a2.c[i,j,0].r*b2.c[i,j,0].r + a2.c[i,j,0].i*b2.c[i,j,0].i;
-        magsq = abs2(ux[i,j,1]) + abs2(uy[i,j,1])
+        magsq = abs2(scale*ux[i,j,1]) + abs2(scale*uy[i,j,1])
         ee = 0.5*conjFactX * magsq / dk;
         Ef[n]+=ee;
         conjFactX=2.0;
