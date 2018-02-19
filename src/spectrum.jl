ae91523150cdb5d21a6cb299e453b345363649d6
Felipe A. V. de Bragança Alves <felipe.avba@gmail.com>
2018-02-18 22:44:16 -0500

    removed no longer necessary scaling from spectra calculation

--- src/spectrum.jl
+++ src/spectrum.jl
@@ -4,7 +4,6 @@
   # Initialize the shells to zeros
   fill!(Ef,0)
   dk = kx[2] - kx[1] 
-  scale = 1/(Nrx*Ny*Nz)
   @inbounds for j=1:Ny
     conjFactX=1.0
     for i=1:Nx
@@ -12,7 +11,7 @@
       if n <= Nx
         #magsq = a1.c[i,j,0].r*b1.c[i,j,0].r + a1.c[i,j,0].i*b1.c[i,j,0].i;
         #magsq += a2.c[i,j,0].r*b2.c[i,j,0].r + a2.c[i,j,0].i*b2.c[i,j,0].i;
-        magsq = abs2(scale*ux[i,j,cplane]) + abs2(scale*uy[i,j,cplane])
+        magsq = abs2(ux[i,j,cplane]) + abs2(uy[i,j,cplane])
         ee = 0.5*conjFactX * magsq / dk;
         Ef[n]+=ee;
         conjFactX=2.0;
