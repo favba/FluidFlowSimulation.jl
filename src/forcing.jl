4c1c100442de764c6d6d51497315a7c793197785
Felipe A. V. de Bragança Alves <felipe.avba@gmail.com>
2018-02-25 21:04:48 -0500

    Finished translating forcing code.

--- src/forcing.jl
+++ src/forcing.jl
@@ -7,7 +7,7 @@
   omega = (2*π/(Tf*ts))
   alpha = getalpha(F)
   eps = Base.eps()
-  _kf = 10.
+  _kf = getKf(F)
 
   Ef = F.Ef
   R = F.R
@@ -27,11 +27,11 @@
   #            factor[i] = sqrt(R[i]/max(Ef[i],eps))*Zf[i]*dt;    
   #          }
   # 
-  for i=1:Nx
+  for i=2:nShells2d
     if kx[i] <= _kf
       R[i] += dt*(-2*alpha*omega*R[i] - omega*omega*(Ef[i] - Em[i])) 
       R[i]=max(0.0, R[i])
-      factor[i] = sqrt(R[i]/max(Ef[i],eps))*Zf[i]*dt     
+      factor[i] = sqrt(R[i]/ifelse(Ef[i]>eps,Ef[i],eps))*Zf[i]*dt     
     end
   end
 
@@ -68,8 +68,24 @@
   #           }
   #     }
 
-
-
+  for j=1:Ny
+    conjFactX=1.0
+    for i=1:Nx
+      K=sqrt(muladd(kx[i],kx[i],ky[j]^2))
+      n = round(Int,K/maxdk) + 1
+      if avgWaveNumInShell2d[n] <= _kf
+        s1[i,j,1] = u1[i,j,1]*factor[n]
+        s2[i,j,1] = u2[i,j,1]*factor[n]
+        ff += (abs2(s1[i,j,1]) + abs2(s2[i,j,1]))*conjFactX
+        fv += (abs2(u1[i,j,1]) + abs2(u2[i,j,1]))*conjFactX
+        conjFactX=2.0
+      end
+    end
+  end
+  ff = ff*0.5/dt
+  fv = fv*0.5/dt
+  horizontalPower = ff+fv
+  powerFraction = 0.99
   # ff = sumToAll(ff)*0.5/dt;
   # fv = sumToAll(fv)/dt;
   # horizontalPower = ff+fv;
@@ -80,10 +96,12 @@
   #   added is (1-powerFraction) of that added to the horizontal */
   # ff=0;
   # fv=0;
+  ff = 0.0
+  fv = 0.0
   # if (_my_pe()==0)
   #   {
   #     umag=0;
-
+  umag = 0.0
   #     // get the sum of the existing velocites
   #     for (k=1; k<=5; k++)
   # {
@@ -91,37 +109,51 @@
   #         ic2=indexc(0,0,Nz-k);
   #         umag += (getUmag(u1, u2, ic)+getUmag(u1, u2, ic2));
   # }
+  
+  for k = 2:6
+    umag += sqrt(abs2(u1[1,1,k]) + abs2(u2[1,1,k])) + sqrt(abs2(u1[1,1,Nz-k+1]) + abs2(u2[1,1,Nz-k+1]))
+  end
 
   #     Float f = dt*sqrt(2.0*horizontalPower*(1.0-powerFraction))/umag;
+  f = dt*sqrt(2.0*horizontalPower*(1.0-powerFraction))/umag
 
   #     for(k=1; k<=5; k++)
+  for k=2:6
   # {
   #         ic=indexc(0,0,k);
   #         s1.c[ic].r = u1.c[ic].r*f;
   #         s1.c[ic].i = u1.c[ic].i*f;
   #         s2.c[ic].r = u2.c[ic].r*f;
   #         s2.c[ic].i = u2.c[ic].i*f;
-
+    s1[1,1,k] = u1[1,1,k]*f
+    s2[1,1,k] = u2[1,1,k]*f
   #         ic2=indexc(0,0,Nz-k);
   #         s1.c[ic2].r = u1.c[ic2].r*f;
   #         s1.c[ic2].i = u1.c[ic2].i*f;
   #         s2.c[ic2].r = u2.c[ic2].r*f;
   #         s2.c[ic2].i = u2.c[ic2].i*f;
-
+    s1[1,1,Nz-k+1] = u1[1,1,Nz-k+1]*f
+    s2[1,1,Nz-k+1] = u2[1,1,Nz-k+1]*f
   #         ff += (s1.c[ic].r*s1.c[ic].r + s1.c[ic].i*s1.c[ic].i + 
   #                 s2.c[ic].r*s2.c[ic].r + s2.c[ic].i*s2.c[ic].i +
   #                 s1.c[ic2].r*s1.c[ic2].r + s1.c[ic2].i*s1.c[ic2].i + 
   #                 s2.c[ic2].r*s2.c[ic2].r + s2.c[ic2].i*s2.c[ic2].i);
-
+    ff += abs2(s1[1,1,k]) + abs2(s2[1,1,k]) + abs2(s1[1,1,Nz-k+1]) + abs2(s2[1,1,Nz-k+1])
   #         fv += (u1.c[ic].r*s1.c[ic].r + u1.c[ic].i*s1.c[ic].i +
   #                 u2.c[ic].r*s2.c[ic].r + u2.c[ic].i*s2.c[ic].i +
   #                 u1.c[ic2].r*s1.c[ic2].r + u1.c[ic2].i*s1.c[ic2].i +
   #                 u2.c[ic2].r*s2.c[ic2].r + u2.c[ic2].i*s2.c[ic2].i);
+    fv += abs2(u1[1,1,k]) + abs2(u2[1,1,k]) + abs2(u1[1,1,Nz-k+1]) + abs2(u2[1,1,Nz-k+1])
   # }
+  end
   #     ff = ff*0.5/dt;
   #     fv = fv/dt;
   #     verticalPower = ff+fv;
   #   }
+  ff = ff*0.5/dt
+  fv = fv/dt
+  verticalPower = ff+fv
   # return 1.0/dt; 
   # }
+  return 1/dt
 end
