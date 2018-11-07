2e574b70156eb7ce96f02917af98cc4bd1e25603
Felipe Alves <favbalves@gmail.com>
2018-11-07 18:30:41 -0500

    Maybe fix dynamic smag

--- src/models.jl
+++ src/models.jl
@@ -254,7 +254,7 @@ end
             if is_dynamic_les(A)
                 L = symouter(û[i],û[i]) - La[i]
                 M = Δ̂²*norm(Sa[i])*Sa[i] - Ma[i]
-                c = max(0.0, 0.5*((traceless(L) : M)/(M:M)))
+                c = max(0.0, -0.5*((traceless(L) : M)/(M:M)))
                 νt = 2*c*Δ²*norm(S)
                 τ[i] = νt*S
                 ca[i] = c
