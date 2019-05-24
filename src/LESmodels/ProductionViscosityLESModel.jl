1a747748781b4baec4d1408a2d0392af10733e54
Felipe Alves <favbalves@gmail.com>
2019-05-24 17:13:16 -0400

    update productionViscosity

--- src/LESmodels/ProductionViscosityLESModel.jl
+++ src/LESmodels/ProductionViscosityLESModel.jl
@@ -1,6 +1,6 @@
 @inline function production_eddy_viscosity(S::SymTen,W::AntiSymTen,c::Real,Δ2::Real)
     a = S:S
     nu = ifelse(a == 0.,zero(eltype(a)),
-         @fastmath max(-ν,c*Δ2*((square(W):S) - (square(S):S))/(2a)))
+         @fastmath max(0.0,c*Δ2*((square(W):S) - (square(S):S))/(2a)))
     return nu
 end
